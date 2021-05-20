6b407267cd24b2d6b6f850995a160c066a30ac2d
Pawel2923 <83728661+Pawel2923@users.noreply.github.com>
2021-05-20 18:15:41 +0200

    changes to index.html slider.js and style.css

--- slider.js
+++ slider.js
@@ -1,16 +1,15 @@
 const image = document.querySelectorAll(".image");
-const container = document.querySelector(".square-container");
 image[0].style.display = "block";
 let currentImg = 0;
 let nextImg = currentImg++;
 
-for (let i = 1; i < image.length; i++) {
-    container.innerHTML += `<div class='square' onclick='changeImage(${i})'></div>`;
+for (let i = 1; i < image.length; i++) {     // Petla dodajaca div .square tyle razy ile jest zdjec
+    document.querySelector(".square-wrapper").innerHTML += `<div class='square' onclick='changeImage(${i})'></div>`;
 }
 
 const square = document.querySelectorAll(".square");
 
-const nextImage = () => {    // Funkcja zmieniajaca obraz na nastepny
+const nextImage = () => {    // Funkcja zmienia obraz na nastepny
     if (nextImg >= image.length)
         nextImg = 0;
 
@@ -72,23 +71,23 @@ document.querySelector(".btn-previous").addEventListener("click", function () {
 nextImage();
 var imageChangeInterval = setInterval(nextImage, 5000);
 
-const changeImage = (index) => {
+const changeImage = (index) => {     // Funkcja zmieniajaca zdjecie przy wybraniu kwadratu
     clearInterval(imageChangeInterval);
     imageChangeInterval = setInterval(nextImage, 5000);
 
     let prvIndex = currentImg;
 
-    if (square[prvIndex].classList.contains("square-selected"))
+    if (square[prvIndex].classList.contains("square-selected")) {
         square[prvIndex].classList.remove("square-selected");
+        image[prvIndex].style.animationName = "fadeOut";
+        image[prvIndex].style.display = "none";
+    }
 
-    image[prvIndex].style.animationName = "fadeOut";
-    image[prvIndex].style.display = "none";
-
-    if (!square[index].classList.contains("square-selected"))
+    if (!square[index].classList.contains("square-selected")) {
         square[index].classList.add("square-selected");
-
-    image[index].style.animationName = "fadeIn";
-    image[index].style.display = "block";
+        image[index].style.animationName = "fadeIn";
+        image[index].style.display = "block";
+    }
 
     currentImg = index;
     nextImg = index + 1;
